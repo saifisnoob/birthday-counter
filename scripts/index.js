a8a0ba33712e7af18b1f548eef6af0b154bcf443
Mohd Alsaif Siddiqui <ialsaifxz@gmail.com>
2021-12-03 23:22:25 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'DEC 04, 2021',
+  name: 'Ramzii'
 };
 
 function hideEverything() {
